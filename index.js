d27fb15caf640d745c1fa6de3675f96610d8b243
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-30 10:26:33 -0500

    server up and running

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require("express");
-const server = express();
+const server = require("./server");
 
-server.use(express.json());
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n** server listening on port ${port} **\n`);
+});
